146a1a8066b50e4ba8711d0bc7687d681634d972
gMariuz <gabriel.mariuz@davinci.edu.ar>
2024-05-05 03:31:39 -0300

    Se agregaron los fetch y las funciones para traer el nombre de 100 pokemones

--- api.js
+++ api.js
@@ -1,4 +1,33 @@
-const urlApi = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-const contenedorTarjetas = document.querySelector("cards");
+
+const contenedorTarjetas = document.getElementById("cards");
 
 
+function buscarPokemones(cant){
+
+    for(let i = 0; i <= cant; i++)
+        buscarPokemon(i);
+}
+
+function buscarPokemon(id){
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then( respuesta => respuesta.json())
+    .then( datos => {crearTarjetas(datos);
+    });
+
+}
+
+
+
+function crearTarjetas(pokemon){
+
+    const nom = document.createElement('p');
+    nom.textContent = pokemon.name;
+
+
+    contenedorTarjetas.appendChild(nom);
+
+}
+
+
+buscarPokemones(100);
